2db27e41f65fe292d09d3a7bfd0c500005cb8e02
DeadShank <37551985+DeadShank@users.noreply.github.com>
2023-08-21 00:19:34 +0300

    up

--- Assets/Scripts/PlayerControlls.cs
+++ Assets/Scripts/PlayerControlls.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControlls : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnEnable() {
+        movement.Enable();
+    }
+
+    private void OnDisable() {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+
+        Debug.Log(horizontalThrow);
+        Debug.Log(verticalThrow);
     }
 }
